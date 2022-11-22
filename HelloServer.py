e4f760b36a94e247d2538a2e9649c4a1bee0bfad
Tomiwa <calebomoniyitomiwa@gmail.com>
2022-11-22 22:08:55 +0100

    Modified the hello server into a server that sends back whatever request path you send it, like an echo.

--- HelloServer.py
+++ HelloServer.py
@@ -16,8 +16,12 @@ class HelloHandler(BaseHTTPRequestHandler):
         self.send_header('Content-type', 'text/plain; charset=utf-8')
         self.end_headers()
 
-        # Now, write the response body.
-        self.wfile.write("Hello, HTTP!\n".encode())
+        # # Now, write the response body.
+        # self.wfile.write("Hello, HTTP!\n".encode())
+
+        # Now, write the response body instance and send variable that contains the request path.
+        self.wfile.write(self.path[1:].encode())
+
 
 if __name__ == '__main__':
     server_address = ('', 8000)  # Serve on all addresses, port 8000.
